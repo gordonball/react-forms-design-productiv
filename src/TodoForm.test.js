b39f1751c73e8d4bb229887f749cc54a2ddaa622
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-17 16:51:42 -0800

    Get rid of unnecessary todo variable

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,13 +1,6 @@
 import { render } from "@testing-library/react";
 import TodoForm from "./TodoForm.js";
 
-const todo = {
-  id: 1,
-  title: "Test",
-  description: "Test",
-  priority: 2
-}
-
 describe("TodoForm", function() {
   it("renders without crashing", function() {
     render(<TodoForm />);
